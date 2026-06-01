ff72e98c95829eeb13a2077d12b4b847b01d2de5
Eduard <eduuardcovrig@gmail.com>
2026-06-02 00:00:01 +0300

    refactor: login, register, welcome screen

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -5,6 +5,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AuthProvider, AuthContext } from './src/context/AuthContext';
 
+import WelcomeScreen from './src/screens/WelcomeScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import RegisterScreen from './src/screens/RegisterScreen';
 import FeedScreen from './src/screens/FeedScreen';
@@ -30,6 +31,7 @@ const RootNavigator = () => {
         </>
       ) : (
         <>
+          <Stack.Screen name="Welcome" component={WelcomeScreen} />
           <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="Register" component={RegisterScreen} />
         </>
